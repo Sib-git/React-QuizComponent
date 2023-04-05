d9471b331048e352402a31780cbd88dd0f0d72df
SIBSANKAR RANA <isibsankarrana@gmail.com>
2023-04-05 18:31:42 +0530

    add QuizQuestion

---
+++ src/QuicQuestion.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class QuizQuestion extends Component {
+    constructor(props ) {
+        super(props);
+        this.state = {
+            quiz_data: {
+                quiz_questions: [
+
+                ],
+            },
+            quiz_position: 1,
+        };
+        
+    }
+     
+
+
+render(){
+    return(
+     <main>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            //quiz question button logic goes here
+            <li>{this.props.quiz_position.answer_options[0]}</li>
+          </ul>
+        </section>
+      </main> 
+    )
+}    
+}
+
+
+export default QuizQuestion;
--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import QuizQuestion from './QuicQuestion';
 
 let quizData = require('./quiz_data.json')
 
@@ -9,9 +10,10 @@ class Quiz extends Component {
   }
   render() {
     return (
-      <div>
-        <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
-      </div>
+      // <div>
+      //   <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+      // </div>
+      <QuizQuestion quiz_question={this.state.quiz_data.quiz_questions[0]} />
     )
   }
 }
