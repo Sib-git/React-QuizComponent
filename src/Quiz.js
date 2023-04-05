bfa6fe0ad1555c76b03ab582f1c9b068d989da75
SIBSANKAR RANA <isibsankarrana@gmail.com>
2023-04-05 15:25:12 +0530

    new changes

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react';
 
-class Quiz extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quiz_position: 1
-    };
-  }
+let quizData = require('./quiz_data.json')
 
+class Quiz extends Component {
+  constructor(props){
+    super(props)
+    this.state = { quiz_position: 1 }
+  }
   render() {
     return (
-      <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
-    )
-
-    const question = quizData.quiz_questions[this.state.quiz_position - 1];
-
-    return (
       <div>
-        <div className="QuizQuestion">
-          {question.instruction_text}
-        </div>
+        <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
       </div>
-    );
+    )
   }
 }
 
